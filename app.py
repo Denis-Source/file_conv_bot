649659fd5515f299c1e1a06d807fa0b349311273
Den <zolotoverkhdenis@gmail.com>
2021-08-29 02:50:44 +0300

    gunicorn added

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from config import Config
 
 app = Flask(__name__)
 api = Api(app)
+api.add_resource(BotResource, "/")
+
 
 if __name__ == '__main__':
-    api.add_resource(BotResource, "/")
-    app.run(host="0.0.0.0", port=Config.SERVER_PORT)
+    app.run(host="0.0.0.0", port=Config.SERVER_PORT, debug=True)
